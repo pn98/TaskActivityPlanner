5279876544ce9233ab474344386559dcc90eb852
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-02-09 17:51:09 +0000

    Default Avatar Profile Picture in Sidebar

--- app/Components/SideBar/SideBar.tsx
+++ app/Components/SideBar/SideBar.tsx
@@ -1,23 +1,33 @@
-"use client";
+"use client"
 
 import React from "react";
 import styled from "styled-components";
+import Image from "next/image"; // Import Image component from next/image
 import { useGlobalState } from "../../Context/globalProvider";
 
 function Sidebar() {
     const { theme } = useGlobalState();
 
     console.log(theme);
-    return <SidebarStyled> Sidebar </SidebarStyled>;
+    return (
+        <SidebarStyled theme={theme}>
+            <div className="profile">
+                <div className="profile-overlay">
+                    {/* Use the imported Image component */}
+                    <Image width={70} height={70} src="/DefaultAvatar.png" alt="profile" />
+                </div>
+            </div>
+        </SidebarStyled>
+    );
 }
 
 const SidebarStyled = styled.nav`
-    width: ${(props) => props.theme.sidebarWidth};
     position: relative;
+    width: 350px; /* Adjust the width here */
     background-color: ${(props) => props.theme.colorBg2};
-    border-right: 2px solid ${(props) => props.theme.borderColor2}; /* Adjust border width */
-    padding-right: 1rem; /* Adjust padding to align content with the border */
-    margin-right: -2px; /* Adjust margin to remove space between border and content */
-    gap: 1rem;
+    border: 2px solid white; /* White border */
+    border-radius: 1rem;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Grey shadow */
 `;
+
 export default Sidebar;
