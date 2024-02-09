b633a9be30f40f5b325e2eb635f4fbe411c77e03
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-02-09 16:29:01 +0000

    ContextProvider.tsx
    Code added demonstrates the usage of the useState and useEffect hooks to manage component state and perform side effects

--- app/Providers/ContextProvider.tsx
+++ app/Providers/ContextProvider.tsx
@@ -8,6 +8,18 @@ interface Props {
 }
 
 function ContextProvider({ children }: Props) {
+    const [isReady, setIsReady] = React. useState(false);
+
+    React.useEffect (() => {
+        setTimeout ( () => { 
+            setIsReady (true);
+        }, 200);
+        }, []);
+    
+        if (!isReady) {
+            return null;
+        }
+
     return <GlobalProvider>{children}</GlobalProvider>
 }
 
