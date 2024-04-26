79adc316d8a2d6aac89923392ef03affe7f79b31
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-04-26 20:06:43 +0100

    Schema Change, route.ts added fields

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,31 +1,31 @@
-import prisma from "@/app/Utils/connect";
-// Importing the Prisma client instance from a custom module "@/app/utils/connect".
-
+import prisma from "@/app/utils/connect";
 import { auth } from "@clerk/nextjs";
-// Importing the auth function from the "@clerk/nextjs" module, possibly for authentication.
-
 import { NextResponse } from "next/server";
-// Importing the NextResponse class from the "next/server" module.
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
-    // Retrieving the userId using the auth function, possibly for authentication.
 
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized", status: 401 });
-      // Returning a NextResponse with a 401 status if userId is falsy.
     }
 
-    const { title, description, date, completed, important } = await req.json();
-    // Extracting task details from the request body.
+    const {
+      title,
+      description,
+      date,
+      completed,
+      priority,
+      mood,
+      workload,
+      timeToComplete,
+    } = await req.json();
 
     if (!title || !description || !date) {
       return NextResponse.json({
         error: "Missing required fields",
         status: 400,
       });
-      // Returning a NextResponse with a 400 status if required fields are missing.
     }
 
     if (title.length < 3) {
@@ -33,7 +33,6 @@ export async function POST(req: Request) {
         error: "Title must be at least 3 characters long",
         status: 400,
       });
-      // Returning a NextResponse with a 400 status if the title is too short.
     }
 
     const task = await prisma.task.create({
@@ -42,31 +41,27 @@ export async function POST(req: Request) {
         description,
         date,
         isCompleted: completed,
-        isImportant: important,
+        completionTime: timeToComplete,
+        priority, // Add the 'priority' property here
+        mood,
+        workload,
         userId,
       },
     });
-    // Creating a new task using Prisma.
 
     return NextResponse.json(task);
-    // Returning a NextResponse with the created task.
   } catch (error) {
     console.log("ERROR CREATING TASK: ", error);
-    // Logging an error if an exception occurs during task creation.
-
     return NextResponse.json({ error: "Error creating task", status: 500 });
-    // Returning a NextResponse with an error message and a 500 status if there's an error.
   }
 }
 
 export async function GET(req: Request) {
   try {
     const { userId } = auth();
-    // Retrieving the userId using the auth function, possibly for authentication.
 
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized", status: 401 });
-      // Returning a NextResponse with a 401 status if userId is falsy.
     }
 
     const tasks = await prisma.task.findMany({
@@ -74,30 +69,21 @@ export async function GET(req: Request) {
         userId,
       },
     });
-    // Retrieving tasks associated with the userId using Prisma.
 
     return NextResponse.json(tasks);
-    // Returning a NextResponse with the retrieved tasks.
   } catch (error) {
     console.log("ERROR GETTING TASKS: ", error);
-    // Logging an error if an exception occurs during task retrieval.
-
     return NextResponse.json({ error: "Error updating task", status: 500 });
-    // Returning a NextResponse with an error message and a 500 status if there's an error.
   }
 }
 
 export async function PUT(req: Request) {
   try {
     const { userId } = auth();
-    // Retrieving the userId using the auth function, possibly for authentication.
-
     const { isCompleted, id } = await req.json();
-    // Extracting task details from the request body.
 
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized", status: 401 });
-      // Returning a NextResponse with a 401 status if userId is falsy.
     }
 
     const task = await prisma.task.update({
@@ -108,15 +94,10 @@ export async function PUT(req: Request) {
         isCompleted,
       },
     });
-    // Updating a task's completion status using Prisma.
 
     return NextResponse.json(task);
-    // Returning a NextResponse with the updated task.
   } catch (error) {
     console.log("ERROR UPDATING TASK: ", error);
-    // Logging an error if an exception occurs during task update.
-
     return NextResponse.json({ error: "Error deleting task", status: 500 });
-    // Returning a NextResponse with an error message and a 500 status if there's an error.
   }
 }
