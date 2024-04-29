6cb3aecc498c7e7d88205f6d378f99fcae5ef31a
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-04-29 20:26:29 +0100

    stripe

---
+++ app/Utils/stripe.ts
@@ -0,0 +1,5 @@
+import Stripe from "stripe";
+const stripe = new Stripe(
+  "sk_test_51P1GteP4PwjJeKutPSUXKktFxFTEYUpCQoZCiUzWJmksEzWPyD2UY4ODYIR1cGAfx6zgYgbaRWvMY8xD2m7S8Wmu00LDKvdh1q"
+);
+export default stripe;
