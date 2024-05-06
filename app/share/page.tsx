e9775a0f39fa20c0249920981fe7508e2995fce0
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-05-06 17:05:52 +0100

    Share page.tsx * Code is clearer and comments added

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React from "react";
 import { useGlobalState } from "../context/globalProvider";
 import TaskItem from "../Components/TaskItem/taskItem";
@@ -7,10 +6,14 @@ import { getPayment, createPayment } from "../actions/payment";
 import { useEffect } from "react";
 import Payment from "../Components/payment";
 
-function Page() {
+// define the Page component
+function page() {
+  // access necessary global state
   const { incompleteTasks, tasks } = useGlobalState();
+  // initialize payment state
   const [payment, setPayment] = React.useState<any>(null);
 
+  // effect to check for incomplete tasks and scroll to them
   useEffect(() => {
     const incompleteTaskExists = tasks.some(
       (task: { isCompleted: any }) => !task.isCompleted
@@ -21,6 +24,7 @@ function Page() {
     }
   }, [tasks]);
 
+  // effect to fetch payment information on component mount
   useEffect(() => {
     const fetchPayment = async () => {
       const { payment, error } = await getPayment();
@@ -36,6 +40,7 @@ function Page() {
     fetchPayment();
   }, []);
 
+  // function to handle payment creation
   const handlePayment = async () => {
     const { payment, error } = await createPayment();
 
@@ -47,105 +52,29 @@ function Page() {
     }
   };
 
+  // render Payment component if payment information is not available
   if (!payment) {
     return <Payment onPaymentSuccess={handlePayment} />;
   }
 
+  // render the page content
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
-      }}
-    >
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
       <table>
         <thead>
           <tr>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Title
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Description
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Date
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Workload
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Priority
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Duration
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Mood
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Actual Workload
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Mood After
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Actual Duration
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Status
-            </th>
-            <th
-              style={{
-                textAlign: "left",
-              }}
-            >
-              Actions
-            </th>
+            <th style={{ textAlign: "left" }}>Title</th>
+            <th style={{ textAlign: "left" }}>Description</th>
+            <th style={{ textAlign: "left" }}>Date</th>
+            <th style={{ textAlign: "left" }}>Workload</th>
+            <th style={{ textAlign: "left" }}>Priority</th>
+            <th style={{ textAlign: "left" }}>Duration</th>
+            <th style={{ textAlign: "left" }}>Mood</th>
+            <th style={{ textAlign: "left" }}>Actual Workload</th>
+            <th style={{ textAlign: "left" }}>Mood After</th>
+            <th style={{ textAlign: "left" }}>Actual Duration</th>
+            <th style={{ textAlign: "left" }}>Status</th>
+            <th style={{ textAlign: "left" }}>Actions</th>
           </tr>
         </thead>
         {incompleteTasks.map((task: any) => (
@@ -156,4 +85,4 @@ function Page() {
   );
 }
 
-export default Page;
+export default page;
