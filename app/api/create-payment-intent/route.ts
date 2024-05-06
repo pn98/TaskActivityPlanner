10bc2a0684a804da3ecc809bf2c1259082729350
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-05-06 16:36:12 +0100

    Payment * Added comments and updated currency

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,17 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
-import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+import { NextRequest, NextResponse } from "next/server"; // importing Next.js server request and response utilities
+import Stripe from "stripe"; // importing Stripe library
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!); // creating a new instance of Stripe
 
+// POST request handler function
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const { amount } = body;
+  const body = await req.json(); // extracting JSON body from the request
+  const { amount } = body; // extracting amount from the request body
   const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    currency: "usd",
+    amount, // specifying payment amount
+    currency: "usd", // specifying currency as USD
     automatic_payment_methods: {
       enabled: true,
     },
   });
 
+  // returning response with client secret for the payment intent
   return NextResponse.json({ clientSecret: paymentIntent.client_secret });
 }
