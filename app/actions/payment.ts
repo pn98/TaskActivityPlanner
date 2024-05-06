c10fe3f187f72440613babe8b729b34cbd75275e
Peter Nicholl <124701525+pn98@users.noreply.github.com>
2024-05-06 16:34:17 +0100

    Comments added to payment.ts

--- app/actions/payment.ts
+++ app/actions/payment.ts
@@ -1,36 +1,40 @@
 "use server";
-import prisma from "@/app/utils/connect";
-import { auth } from "@clerk/nextjs";
+import prisma from "@/app/utils/connect"; // importing Prisma client
+import { auth } from "@clerk/nextjs"; // importing authentication middleware
 
+// function to retrieve payment details
 export async function getPayment() {
   try {
-    const { userId } = auth();
+    const { userId } = auth(); // extracting user ID from authentication token
 
     if (!userId) {
-      return { error: "Unauthorized", status: 401 };
+      return { error: "unauthorized", status: 401 }; // return unauthorized error if user ID is missing
     }
 
+    // querying payment details for the user
     const payment = await prisma.payment.findFirst({
       where: {
         userId: userId,
       },
     });
 
-    return { payment };
+    return { payment }; // returning payment details
   } catch (error) {
-    console.log("ERROR GETTING PAYMENT: ", error);
-    return { error: "Error getting payment", status: 500 };
+    console.log("error getting payment: ", error); // logging error if encountered while fetching payment
+    return { error: "error getting payment", status: 500 }; // returning error response
   }
 }
 
+// function to create a new payment
 export async function createPayment() {
   try {
-    const { userId } = auth();
+    const { userId } = auth(); // extracting user ID from authentication token
 
     if (!userId) {
-      return { error: "Unauthorized", status: 401 };
+      return { error: "unauthorized", status: 401 }; // return unauthorized error if user ID is missing
     }
 
+    // creating a new payment record for the user
     const payment = await prisma.payment.create({
       data: {
         paymentDone: true,
@@ -38,9 +42,9 @@ export async function createPayment() {
       },
     });
 
-    return { payment };
+    return { payment }; // returning newly created payment details
   } catch (error) {
-    console.log("ERROR CREATING PAYMENT: ", error);
-    return { error: "Error creating payment", status: 500 };
+    console.log("error creating payment: ", error); // logging error if encountered while creating payment
+    return { error: "error creating payment", status: 500 }; // returning error response
   }
 }
